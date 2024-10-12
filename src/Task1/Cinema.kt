aef0ddf6f8f9eb5e90b8493f91c32158aaf14269
ярослав <yaracrut245@gmail.com>
2024-10-12 14:36:32 +0500

    1

--- src/Task1/Cinema.kt
+++ src/Task1/Cinema.kt
@@ -1,4 +1,10 @@
 package Task1
 
-class Cinema {
+class Cinema (val name : String){
+
+    class FilmArchive(val name:String, val time: String, val place : String ){
+        override fun toString(): String {
+            return "Название фильма:$name\n Время сеанса:$time\n Место: $place)"
+        }
+    }
 }
--- src/Task1/Main.kt
+++ src/Task1/Main.kt
@@ -1,2 +1,22 @@
 package Task1
 
+fun main (){
+    val cinema = Cinema ("Каро")
+    var arrayOfFilmArchive : Array<Cinema.FilmArchive> = arrayOf()
+
+    println("Заполнить форму для выдачи билета\n 1.Да \n 2.Нет")
+    if (readln() == "1"){
+        while (true){
+            println("Введите название фильма, время сеанса и место")
+            val name = readln()
+            val time = readln()
+            val place = readln()
+            arrayOfFilmArchive += Cinema.FilmArchive(name, time, place)
+            println("Продолжить: \n1.Да \n2.Нет")
+            if (readln() == "2") break
+        }
+    }
+    println("Архив проданных билето кинотеатра: ${cinema.name}")
+    if (arrayOfFilmArchive.isNotEmpty()) println(arrayOfFilmArchive.contentToString())
+    else println("Архив пуст")
+}
